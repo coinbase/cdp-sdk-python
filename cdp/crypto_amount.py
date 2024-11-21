47c1f4cc768e7996408f03e34836baf90c94c9c5
Rohan Agarwal <rohan.agarwal.1@coinbase.com>
2024-11-21 16:05:55 -0500

    Fix

--- cdp/crypto_amount.py
+++ cdp/crypto_amount.py
@@ -1,13 +1,14 @@
 from decimal import Decimal
 
 from cdp.asset import Asset
+from cdp.client.models.crypto_amount import CryptoAmount as CryptoAmountModel
 
 
 class CryptoAmount:
     """A representation of a CryptoAmount that includes the amount and asset."""
 
     def __init__(self, amount: Decimal, asset: Asset, asset_id: str | None = None) -> None:
-        """Initialize a new CryptoAmount.
+        """Initialize a new CryptoAmount. Do not use this directly, use the from_model or from_model_and_asset_id methods instead.
 
         Args:
             amount (Decimal): The amount of the Asset
@@ -20,37 +21,37 @@ class CryptoAmount:
         self._asset_id = asset_id or asset.asset_id
 
     @classmethod
-    def from_model(cls, amount_model) -> "CryptoAmount":
+    def from_model(cls, crypto_amount_model: CryptoAmountModel) -> "CryptoAmount":
         """Convert a CryptoAmount model to a CryptoAmount.
 
         Args:
-            amount_model: The crypto amount from the API.
+            amount_model (CryptoAmountModel): The crypto amount from the API.
 
         Returns:
             CryptoAmount: The converted CryptoAmount object.
 
         """
-        asset = Asset.from_model(amount_model.asset)
-        return cls(amount=asset.from_atomic_amount(amount_model.amount), asset=asset)
+        asset = Asset.from_model(crypto_amount_model.asset)
+        return cls(amount=asset.from_atomic_amount(crypto_amount_model.amount), asset=asset)
 
     @classmethod
-    def from_model_and_asset_id(cls, amount_model, asset_id: str) -> "CryptoAmount":
+    def from_model_and_asset_id(cls, crypto_amount_model: CryptoAmountModel, asset_id: str) -> "CryptoAmount":
         """Convert a CryptoAmount model and asset ID to a CryptoAmount.
 
         This can be used to specify a non-primary denomination that we want the amount
         to be converted to.
 
         Args:
-            amount_model: The crypto amount from the API.
+            amount_model (CryptoAmountModel): The crypto amount from the API.
             asset_id (str): The Asset ID of the denomination we want returned.
 
         Returns:
             CryptoAmount: The converted CryptoAmount object.
 
         """
-        asset = Asset.from_model(amount_model.asset, asset_id=asset_id)
+        asset = Asset.from_model(crypto_amount_model.asset, asset_id=asset_id)
         return cls(
-            amount=asset.from_atomic_amount(amount_model.amount), asset=asset, asset_id=asset_id
+            amount=asset.from_atomic_amount(crypto_amount_model.amount), asset=asset, asset_id=asset_id
         )
 
     @property
